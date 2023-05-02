b6c1b03297dabd677448aebd6089a7a98f34d382
MainakRepositor <sultankhilji001@gmail.com>
2023-05-02 16:34:25 +0530

    added files

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System |<a href="https://mainakfolio.netlify.app/" target="_blank"  > Designed by : MWS</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
